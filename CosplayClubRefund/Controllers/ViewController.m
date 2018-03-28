f76d0fdccedc3ae913a506360dccd501fdd92f2f
moxcomic <moxcomic@gmail.com>
2018-03-28 15:40:46 +0800

    test

--- CosplayClubRefund/Controllers/ViewController.m
+++ CosplayClubRefund/Controllers/ViewController.m
@@ -17,7 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    View.layer.borderColor = [UIColor blackColor].CGColor;
+    View.layer.borderWidth = 2;
 }
 
 - (IBAction)addAction:(id)sender {
